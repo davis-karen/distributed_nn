3e6ae644d368b60120b77a149c9fd37001bb823a
Karen Davis <karend@thoughtworks.com>
2019-11-21 15:05:09 +1100

    Adding in a test function and tests for the test function

--- distributed_model/train.py
+++ distributed_model/train.py
@@ -1,3 +1,4 @@
+import torch
 from torch.nn import Module
 from torch.nn.functional import nll_loss, log_softmax
 
@@ -13,3 +14,21 @@ def train(model: Module, epochs: int, data_loader, optimiser, loss_function):
             optimiser.step() # applys the gradients to the weights
             if epoch % 10 == 0:
                 print(f'Epoch {epoch} Batch : {batch_id} Loss: {loss.item()}')
+
+
+def test(model: Module, data_loader, loss_function):
+    model.eval() #letting pytorch know this is evaluation phase
+    test_loss = 0
+    accuracy = 0
+    with torch.no_grad(): # don't calculate gradients
+        for X, y in data_loader:
+            print('hi')
+            output = model(X)
+            test_loss += loss_function(output, y).item()
+            pred = output.max(1)[1]
+            accuracy += pred.eq(y).sum().item()
+
+    test_loss /= len(data_loader.dataset)
+    accuracy /= len(data_loader.dataset)
+    print(f'Test set: Average loss: {test_loss} | Accuracy: {accuracy}')
+    return test_loss, accuracy
--- test/test_train.py
+++ test/test_train.py
@@ -3,6 +3,7 @@ import torch
 
 from distributed_model.model import DistributedMLP
 from distributed_model.train import train
+from distributed_model.train import test
 from torch.optim import SGD
 from torch.utils.data import DataLoader
 from torch.utils.data import TensorDataset
@@ -16,6 +17,10 @@ class TestTrain:
                    np.float32))
 
     X_test = torch.from_numpy(np.asarray([[1, 1], [0, 1], [0, 0], [1, 1], [1, 0], [1, 1]], np.float32))
+
+    y_test_0 = torch.from_numpy(np.asarray([[0], [0], [0], [0], [0], [0]], np.float32))
+
+    y_test_1 = torch.from_numpy(np.asarray([[1], [1], [1], [1], [1], [1]], np.float32))
 
     y = torch.from_numpy(np.asarray([[0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0]], np.float32)).squeeze(
         1)
@@ -58,3 +63,18 @@ class TestTrain:
         assert y_pred is not None
         for y in y_pred:
             assert 1 - y < 1e-2
+
+    def test_test_model(self):
+        mlp = DistributedMLP(2, (3, 3), 1)
+        optimizer = SGD(mlp.parameters(), lr=0.001, momentum=0.9)
+        loss_function = torch.nn.L1Loss()
+        dataset = TensorDataset(self.X, self.y_1)
+        data_loader = DataLoader(dataset)
+
+        test_dataset = TensorDataset(self.X_test, self.y_test_1)
+        test_data_loader = DataLoader(test_dataset)
+
+        train(mlp, 100, data_loader, optimizer, loss_function)
+        test_loss, accuracy = test(mlp,  test_data_loader, loss_function)
+        assert test_loss is not None
+        assert accuracy is not None
